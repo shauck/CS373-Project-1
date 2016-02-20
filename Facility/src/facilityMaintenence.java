ecc8fc8e249fa2b5b214c21a52691ee179d5c0d7
samhauck <shauck@luc.edu>
2016-02-20 17:30:12 -0600

    deleted routine maintenence class

--- Facility/src/facilityMaintenence.java
+++ Facility/src/facilityMaintenence.java
@@ -1,8 +1,6 @@
 /**
  * Created by samhauck on 2/20/16.
  */
-public class facilityMaintence extends Facility {
-    public facilityMaintence(){
-        super
-    }
+public class facilityMaintenence {
+    
 }
--- Facility/src/routineMaintenence.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by samhauck on 2/20/16.
- */
-public class routineMaintenence extends Facility {
-
-    public String routineMaintenence(Facility facility) {
-            return "Maintenence for " + facility + "is scheduled for every " + maintenenceSchedule;
-    }
-}
